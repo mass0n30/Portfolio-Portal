b010b90d335ac13b71fb692548cdf2246a8428ac
masson <65047703+mass0n30@users.noreply.github.com>
2026-09-09 10:26:54 -0400

    Initial commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+
+{/* import { useState, useEffect } from 'react' */}
+{/*maybe import local styles */}
+
+function HomePage() {
+
+  return (
+    <>
+      <div>Page One</div>
+    </>
+  )
+}
+
+export default HomePage;
